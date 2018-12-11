42acff8a68ed83a014f1d1e9f65d018923196039
ivakavankova <kavankova.iva@gmail.com>
2018-12-11 20:42:42 +0100

    added tables

--- src/components/xkavan05.vue
+++ src/components/xkavan05.vue
@@ -1,7 +1,157 @@
 <template>
     <div class="container">
         <div class="picker">
-            Iva
+            <div class="tabulka1">
+                <div class="cell">1</div>
+                <div class="cell">2</div>
+                <div class="cell">3</div>
+                <div class="cell">4</div>
+                <div class="cell">5</div>
+                <div class="cell">6</div>
+                <div class="cell">7</div>
+                <div class="cell">8</div>
+                <div class="cell">9</div>
+                <div class="cell">10</div>
+                <div class="cell">11</div>
+                <div class="cell">12</div>
+                <div class="cell">13</div>
+                <div class="cell">14</div>
+                <div class="cell">15</div>
+                <div class="cell">16</div>
+                <div class="cell">17</div>
+                <div class="cell">18</div>
+                <div class="cell">19</div>
+                <div class="cell">20</div>
+                <div class="cell">21</div>
+                <div class="cell">22</div>
+                <div class="cell">23</div>
+                <div class="cell">24</div>
+            </div>
+
+            <div class="tabulka2">
+                <div class="cell">1</div>
+                <div class="cell">2</div>
+                <div class="cell">3</div>
+                <div class="cell">4</div>
+                <div class="cell">5</div>
+                <div class="cell">6</div>
+                <div class="cell">7</div>
+                <div class="cell">8</div>
+                <div class="cell">9</div>
+                <div class="cell">10</div>
+                <div class="cell">11</div>
+                <div class="cell">12</div>
+                <div class="cell">13</div>
+                <div class="cell">14</div>
+                <div class="cell">15</div>
+                <div class="cell">16</div>
+                <div class="cell">17</div>
+                <div class="cell">18</div>
+                <div class="cell">19</div>
+                <div class="cell">20</div>
+                <div class="cell">21</div>
+                <div class="cell">22</div>
+                <div class="cell">23</div>
+                <div class="cell">24</div>
+                <div class="cell">25</div>
+                <div class="cell">26</div>
+                <div class="cell">27</div>
+                <div class="cell">28</div>
+                <div class="cell">29</div>
+                <div class="cell">30</div>
+                <div class="cell">31</div>
+                <div class="cell">32</div>
+                <div class="cell">33</div>
+                <div class="cell">34</div>
+                <div class="cell">35</div>
+                <div class="cell">36</div>
+                <div class="cell">37</div>
+                <div class="cell">38</div>
+                <div class="cell">39</div>
+                <div class="cell">40</div>
+                <div class="cell">41</div>
+                <div class="cell">42</div>
+                <div class="cell">43</div>
+                <div class="cell">44</div>
+                <div class="cell">45</div>
+                <div class="cell">46</div>
+                <div class="cell">47</div>
+                <div class="cell">48</div>
+                <div class="cell">49</div>
+                <div class="cell">50</div>
+                <div class="cell">51</div>
+                <div class="cell">52</div>
+                <div class="cell">53</div>
+                <div class="cell">54</div>
+                <div class="cell">55</div>
+                <div class="cell">56</div>
+                <div class="cell">57</div>
+                <div class="cell">58</div>
+                <div class="cell">59</div>
+            </div>
+
+            <div class="tabulka3">
+                <div class="cell">1</div>
+                <div class="cell">2</div>
+                <div class="cell">3</div>
+                <div class="cell">4</div>
+                <div class="cell">5</div>
+                <div class="cell">6</div>
+                <div class="cell">7</div>
+                <div class="cell">8</div>
+                <div class="cell">9</div>
+                <div class="cell">10</div>
+                <div class="cell">11</div>
+                <div class="cell">12</div>
+                <div class="cell">13</div>
+                <div class="cell">14</div>
+                <div class="cell">15</div>
+                <div class="cell">16</div>
+                <div class="cell">17</div>
+                <div class="cell">18</div>
+                <div class="cell">19</div>
+                <div class="cell">20</div>
+                <div class="cell">21</div>
+                <div class="cell">22</div>
+                <div class="cell">23</div>
+                <div class="cell">24</div>
+                <div class="cell">25</div>
+                <div class="cell">26</div>
+                <div class="cell">27</div>
+                <div class="cell">28</div>
+                <div class="cell">29</div>
+                <div class="cell">30</div>
+                <div class="cell">31</div>
+                <div class="cell">32</div>
+                <div class="cell">33</div>
+                <div class="cell">34</div>
+                <div class="cell">35</div>
+                <div class="cell">36</div>
+                <div class="cell">37</div>
+                <div class="cell">38</div>
+                <div class="cell">39</div>
+                <div class="cell">40</div>
+                <div class="cell">41</div>
+                <div class="cell">42</div>
+                <div class="cell">43</div>
+                <div class="cell">44</div>
+                <div class="cell">45</div>
+                <div class="cell">46</div>
+                <div class="cell">47</div>
+                <div class="cell">48</div>
+                <div class="cell">49</div>
+                <div class="cell">50</div>
+                <div class="cell">51</div>
+                <div class="cell">52</div>
+                <div class="cell">53</div>
+                <div class="cell">54</div>
+                <div class="cell">55</div>
+                <div class="cell">56</div>
+                <div class="cell">57</div>
+                <div class="cell">58</div>
+                <div class="cell">59</div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -30,4 +180,19 @@
         height: 70vh;
         background-color: white;
     }
+
+    .tabulka1 .tabulka2 .tabulka3 {
+        background: #fff;
+        margin: 0 auto;
+        width: 300px;
+        padding: 10px;
+    }
+
+    .cell {
+        display: inline-block;
+        width: 30px;
+        border: 1px solid;
+        text-align: center;
+    }
+
 </style>
