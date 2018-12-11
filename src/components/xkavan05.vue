183efd5c131f5826ddb4d648931a1fc65befaccf
ivakavankova <kavankova.iva@gmail.com>
2018-12-11 23:15:11 +0100

    done

--- src/components/xkavan05.vue
+++ src/components/xkavan05.vue
@@ -1,171 +1,83 @@
 <template>
-    <div class="container">
-        <div class="picker">
-            <div class="tabulka1">
-                <div class="cell">1</div>
-                <div class="cell">2</div>
-                <div class="cell">3</div>
-                <div class="cell">4</div>
-                <div class="cell">5</div>
-                <div class="cell">6</div>
-                <div class="cell">7</div>
-                <div class="cell">8</div>
-                <div class="cell">9</div>
-                <div class="cell">10</div>
-                <div class="cell">11</div>
-                <div class="cell">12</div>
-                <div class="cell">13</div>
-                <div class="cell">14</div>
-                <div class="cell">15</div>
-                <div class="cell">16</div>
-                <div class="cell">17</div>
-                <div class="cell">18</div>
-                <div class="cell">19</div>
-                <div class="cell">20</div>
-                <div class="cell">21</div>
-                <div class="cell">22</div>
-                <div class="cell">23</div>
-                <div class="cell">24</div>
-            </div>
+    <div class="selector" @click="submit">
+            <div class="picker" @click.stop>
+                <div class="tabulka1">
+                    <div class="radek" v-for="(data, index) in Array(4)" :key="index">
 
-            <div class="tabulka2">
-                <div class="cell">1</div>
-                <div class="cell">2</div>
-                <div class="cell">3</div>
-                <div class="cell">4</div>
-                <div class="cell">5</div>
-                <div class="cell">6</div>
-                <div class="cell">7</div>
-                <div class="cell">8</div>
-                <div class="cell">9</div>
-                <div class="cell">10</div>
-                <div class="cell">11</div>
-                <div class="cell">12</div>
-                <div class="cell">13</div>
-                <div class="cell">14</div>
-                <div class="cell">15</div>
-                <div class="cell">16</div>
-                <div class="cell">17</div>
-                <div class="cell">18</div>
-                <div class="cell">19</div>
-                <div class="cell">20</div>
-                <div class="cell">21</div>
-                <div class="cell">22</div>
-                <div class="cell">23</div>
-                <div class="cell">24</div>
-                <div class="cell">25</div>
-                <div class="cell">26</div>
-                <div class="cell">27</div>
-                <div class="cell">28</div>
-                <div class="cell">29</div>
-                <div class="cell">30</div>
-                <div class="cell">31</div>
-                <div class="cell">32</div>
-                <div class="cell">33</div>
-                <div class="cell">34</div>
-                <div class="cell">35</div>
-                <div class="cell">36</div>
-                <div class="cell">37</div>
-                <div class="cell">38</div>
-                <div class="cell">39</div>
-                <div class="cell">40</div>
-                <div class="cell">41</div>
-                <div class="cell">42</div>
-                <div class="cell">43</div>
-                <div class="cell">44</div>
-                <div class="cell">45</div>
-                <div class="cell">46</div>
-                <div class="cell">47</div>
-                <div class="cell">48</div>
-                <div class="cell">49</div>
-                <div class="cell">50</div>
-                <div class="cell">51</div>
-                <div class="cell">52</div>
-                <div class="cell">53</div>
-                <div class="cell">54</div>
-                <div class="cell">55</div>
-                <div class="cell">56</div>
-                <div class="cell">57</div>
-                <div class="cell">58</div>
-                <div class="cell">59</div>
-            </div>
+                        <div v-for="(data, j) in Array(6)" :key="j">
+                            <div class="bunka"
+                                 v-on:click="hour = index * 6 + j"
+                                 v-bind:class="{selected: hour === index * 6 + j}">
+                                {{ index * 6 + j }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-            <div class="tabulka3">
-                <div class="cell">1</div>
-                <div class="cell">2</div>
-                <div class="cell">3</div>
-                <div class="cell">4</div>
-                <div class="cell">5</div>
-                <div class="cell">6</div>
-                <div class="cell">7</div>
-                <div class="cell">8</div>
-                <div class="cell">9</div>
-                <div class="cell">10</div>
-                <div class="cell">11</div>
-                <div class="cell">12</div>
-                <div class="cell">13</div>
-                <div class="cell">14</div>
-                <div class="cell">15</div>
-                <div class="cell">16</div>
-                <div class="cell">17</div>
-                <div class="cell">18</div>
-                <div class="cell">19</div>
-                <div class="cell">20</div>
-                <div class="cell">21</div>
-                <div class="cell">22</div>
-                <div class="cell">23</div>
-                <div class="cell">24</div>
-                <div class="cell">25</div>
-                <div class="cell">26</div>
-                <div class="cell">27</div>
-                <div class="cell">28</div>
-                <div class="cell">29</div>
-                <div class="cell">30</div>
-                <div class="cell">31</div>
-                <div class="cell">32</div>
-                <div class="cell">33</div>
-                <div class="cell">34</div>
-                <div class="cell">35</div>
-                <div class="cell">36</div>
-                <div class="cell">37</div>
-                <div class="cell">38</div>
-                <div class="cell">39</div>
-                <div class="cell">40</div>
-                <div class="cell">41</div>
-                <div class="cell">42</div>
-                <div class="cell">43</div>
-                <div class="cell">44</div>
-                <div class="cell">45</div>
-                <div class="cell">46</div>
-                <div class="cell">47</div>
-                <div class="cell">48</div>
-                <div class="cell">49</div>
-                <div class="cell">50</div>
-                <div class="cell">51</div>
-                <div class="cell">52</div>
-                <div class="cell">53</div>
-                <div class="cell">54</div>
-                <div class="cell">55</div>
-                <div class="cell">56</div>
-                <div class="cell">57</div>
-                <div class="cell">58</div>
-                <div class="cell">59</div>
-            </div>
+                <div class="tabulka2">
+                    <div class="radek" v-for="(data, index) in Array(6)" :key="index">
 
+                        <div v-for="(data, j) in Array(10)" :key="j">
+                            <div class="bunka"
+                                v-on:click="minute = index * 10 + j"
+                                v-bind:class="{selected: minute === index * 10 + j}">
+                                {{ index * 10+ j }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="tabulka3">
+                    <div class="radek" v-for="(data, index) in Array(6)" :key="index">
+
+                        <div v-for="(data, j) in Array(10)" :key="j">
+                            <div class="bunka"
+                                v-on:click="second = index * 10 + j"
+                                v-bind:class="{selected: second === index * 10 + j}">
+                                {{ index * 10 + j }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            <div class=result> {{('0' + hour).slice(-2)}}:{{('0' + minute).slice(-2)}}:{{('0' + second).slice(-2)}} </div>
+            <div class="submit" @click="submit">Submit</div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "xkavan05"
+        name: "xkavan05",
+        data() {
+            if (this.value instanceof Date) {
+                return {
+                    hour: this.value.getHours(),
+                    minute: this.value.getMinutes(),
+                    second: this.value.getSeconds()
+                }
+            }
+            return {
+                hour: 0,
+                minute: 0,
+                second: 0
+            }
+        },
+
+        props: {value: {}},
+        methods: {
+            submit() {
+                this.$emit('submit');
+                this.$emit('input', new Date(1970, 0, 0, this.hour, this.minute, this.second, 0))
+            }
+        }
     }
 </script>
 
 <style scoped>
-    .container {
-        background-color: #666666bb;
+    .selector {
         position: fixed;
+        background-color: #bbbbbb66;
         top: 0;
         bottom: 0;
         left: 0;
@@ -173,26 +85,76 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        flex-direction: column-reverse;
     }
 
     .picker {
         width: 70vh;
-        height: 70vh;
+        height: 75vh;
         background-color: white;
     }
 
-    .tabulka1 .tabulka2 .tabulka3 {
+    .radek {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .tabulka1 {
+        flex-direction: column;
+        display: flex;
+        background: #fff;
+        margin: 0 auto;
+        width: 180px;
+        padding: 10px;
+    }
+
+    .tabulka2 {
+        flex-direction: column;
+        display: flex;
         background: #fff;
         margin: 0 auto;
         width: 300px;
         padding: 10px;
     }
 
-    .cell {
-        display: inline-block;
+    .tabulka3 {
+        flex-direction: column;
+        display: flex;
+        background: #fff;
+        margin: 0 auto;
+        width: 300px;
+        padding: 10px;
+    }
+
+    .bunka {
         width: 30px;
         border: 1px solid;
         text-align: center;
+        background-color: #00BFFF;
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+        -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none;
+    }
+
+    .selected {
+        background-color: red;
+
+    }
+
+    .submit {
+        background-color: dodgerblue;
+        margin-top: 10px;
+
+        color: white;
+        cursor: pointer;
+        padding: 1em;
+    }
+
+    .result {
+        font-size: 70px;
     }
 
 </style>
